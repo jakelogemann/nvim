20e86afe5d946bb9cab6af1340cac18c0a11bdcc
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2025-08-15 23:27:22 -0400

    refactor(cmp): simplify structure to improve readability of configuration

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,83 +1,80 @@
 return {
+  "hrsh7th/nvim-cmp",
+  lazy = true,
+  enabled = true,
+  event = "UiEnter",
 
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = true,
-    enabled = true,
-    event = "UiEnter",
-
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = { "zbirenbaum/copilot.lua" },
-        config = function() require("copilot_cmp").setup() end,
-      },
+  -- these dependencies will only be loaded when cmp loads
+  -- dependencies are always lazy-loaded unless specified otherwise
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    {
+      "zbirenbaum/copilot-cmp",
+      dependencies = { "zbirenbaum/copilot.lua" },
+      config = function() require("copilot_cmp").setup() end,
     },
-
-    opts = function(_, opts)
-      local cmp = require "cmp"
-      opts.sources = cmp.config.sources({
-        { name = "copilot" },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-        { name = "path" },
-      })
-      -- Give Copilot higher priority and place its items at the top
-      opts.sorting = opts.sorting or {}
-      opts.sorting.priority_weight = 2
-      opts.sorting.comparators = {
-        require("copilot_cmp.comparators").prioritize,
-        cmp.config.compare.offset,
-        cmp.config.compare.exact,
-        cmp.config.compare.score,
-        cmp.config.compare.recently_used,
-        cmp.config.compare.locality,
-        cmp.config.compare.kind,
-        cmp.config.compare.sort_text,
-        cmp.config.compare.length,
-        cmp.config.compare.order,
-      }
-      opts.snippet = {
-        expand = function(args) require("luasnip").lsp_expand(args.body) end,
-      }
-      opts.mapping = cmp.mapping.preset.insert {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif package.loaded["luasnip"] and require("luasnip").expand_or_jumpable() then
-            require("luasnip").expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif package.loaded["luasnip"] and require("luasnip").jumpable(-1) then
-            require("luasnip").jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }
-
-      return opts
-    end,
   },
+
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    opts.sources = cmp.config.sources({
+      { name = "copilot" },
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    }, {
+      { name = "buffer" },
+      { name = "path" },
+    })
+    -- Give Copilot higher priority and place its items at the top
+    opts.sorting = opts.sorting or {}
+    opts.sorting.priority_weight = 2
+    opts.sorting.comparators = {
+      require("copilot_cmp.comparators").prioritize,
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    }
+    opts.snippet = {
+      expand = function(args) require("luasnip").lsp_expand(args.body) end,
+    }
+    opts.mapping = cmp.mapping.preset.insert {
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      },
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif package.loaded["luasnip"] and require("luasnip").expand_or_jumpable() then
+          require("luasnip").expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif package.loaded["luasnip"] and require("luasnip").jumpable(-1) then
+          require("luasnip").jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }
+
+    return opts
+  end,
 }
