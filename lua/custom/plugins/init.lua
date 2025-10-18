6eb8062007e41b622e2afbabd33e0c613ee0b9b8
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2025-10-18 11:58:56 -0400

    add LuaPad

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,6 +6,25 @@ return {
     config = function() vim.cmd.colorscheme "catppuccin-macchiato" end,
   },
   {
+    "rafcamlet/nvim-luapad",
+    lazy = true,
+    cmd = { "Lua", "Luapad", "LuaRun" },
+    opts = {
+      count_limit = 150000,
+      error_indicator = false,
+      eval_on_move = true,
+      error_highlight = 'WarningMsg',
+      split_orientation = 'horizontal',
+      on_init = function()
+        print 'Hello from Luapad!'
+      end,
+      context = {
+        the_answer = 42,
+        shout = function(str) return(string.upper(str) .. '!') end
+      }
+    },
+  },
+  {
     "zbirenbaum/copilot.lua",
     lazy = true,
     event = "InsertEnter",
